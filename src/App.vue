7e38a0540c31b79002ca0105cde7127cd42157a4
nambt-2950 <bui.thanh.nam@sun-asterisk.com>
2022-02-28 00:21:07 +0700

    demo dark mode with tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex w-full ml-3 mb-3 mt-3">
+		<label for="toggleTheme" class="flex items-center cursor-pointer">
+			<div class="relative">
+				<input 
+          type="checkbox" 
+          id="toggleTheme" 
+          class="sr-only" 
+          @change="check($event)" 
+          :checked="theme=='dark' ? true : false"
+        >
+				<div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
+				<div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+			</div>
+			<div class="ml-3 text-gray-700 font-medium">
+				Dark mode
+			</div>
+  		</label>
+	</div>
+
+  <Post />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from './components/Post.vue'
 
 export default {
   name: 'App',
+  data: function() {
+    return {
+      theme: ''
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Post
+  },
+  methods: {
+    check() {
+      if (this.theme !== 'dark') {
+        this.theme = 'dark'
+        document.documentElement.classList.add('dark')
+        localStorage.theme = 'dark'
+      } else {
+        this.theme = ''
+        document.documentElement.classList.remove('dark')
+        localStorage.theme = ''
+      }
+    }
+  },
+  mounted() {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      this.theme = 'dark'
+      document.documentElement.classList.add('dark')
+    } else {
+      this.theme = ''
+      document.documentElement.classList.remove('dark')
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+input:checked ~ .dot {
+  transform: translateX(100%);
+  background-color: #48bb78;
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="bg-white dark:bg-slate-900 px-6 py-8 ring-1 ring-slate-900/5">
+		<h2 class="text-slate-900 dark:text-white mt-5 text-3xl font-medium tracking-tight">Writes Upside-Down</h2>
+		<p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+			The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+		</p>
+	</div>
+</template>
+
+<style scoped>
+input:checked ~ .dot {
+  transform: translateX(100%);
+  background-color: #48bb78;
+}
+</style>
